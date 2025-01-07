c2b76bd4d560a2df0fe6a98191cbce489c5b2ec8
Mustafa Khericha <mustafakhericha89@gmail.com>
2025-01-07 17:24:01 +0530

    Update q11.py

--- q11.py
+++ q11.py
@@ -1,11 +1,3 @@
 li = [1,2,3,4,5]
 print(sum(li))
-
-#baki
-def fun(li):
-    for i in range(len(li)):
-        if li[i] > li[i+1]:
-            return li[i]
-            
-k = fun(li)
-print(k)
+print(max(li))
